52d84514c80bb329d279fca491ff87228e7a755d
jcdororo <wnwlcks123@gmail.com>
2022-05-24 11:30:38 +0900

    콘솔을 추가했습니다

--- main/JeinTalk.java
+++ main/JeinTalk.java
@@ -2,6 +2,7 @@ public class JeinTalk{
 	public static void main(String[] args){
 		System.out.println("jein talk start");
 		System.out.println("로컬에서 변경한 파일 Fork로 저장");
+		System.out.println("주지찬이 추가한 라인 입니다.");
 	}
 }
 
