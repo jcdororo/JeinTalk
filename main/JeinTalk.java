f645362a64f84879ae94cceb33f914cb252bcd12
jcdororo <wnwlcks123@gmail.com>
2022-05-26 10:21:05 +0900

    커밋 01

--- main/JeinTalk.java
+++ main/JeinTalk.java
@@ -3,6 +3,8 @@ public class JeinTalk{
 		System.out.println("jein talk start");
 		System.out.println("로컬에서 변경한 파일 Fork로 저장");
 		System.out.println("주지찬이 추가한 라인 입니다.");
+		
+		System.out.println("주지찬이 한번 더 추가한 라인 입니다.");
 	}
 }
 
